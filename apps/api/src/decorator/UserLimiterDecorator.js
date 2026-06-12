f63b0628ab0cc34bf38c31f9fb46829ff7075829
Harsh Vardhan <harshror2006@gmail.com>
2026-06-12 10:29:09 +0530

    feat: add UserLimiter to enforce per-user token buket limits

---
+++ apps/api/src/decorator/UserLimiterDecorator.js
@@ -0,0 +1,28 @@
+import {BaseLimiterDecorator} from './BaseLimiterDecorator.js'
+import {TokenBucketLimiter} from '../algorithms/TokenBucketLimiter.js'
+
+export class UserLimiterDecorator extends BaseLimiterDecorator{
+    
+    constructor(wrappedLimiter,userLimits){
+        super(wrappedLimiter);
+        this.userLimiter = new TokenBucketLimiter(userLimits);
+    }
+
+    async isAllowed(req,options){
+        if(!req.userId){
+            return this.wrappedLimiter.isAllowed(req,options);
+        }
+        const userKey = `user:${req.userId}`;
+        const userResult = await this.userLimiter.isAllowed(userKey);
+
+        if(!userResult.allowed){
+            return{
+                allowed:false,
+                remaining:userResult.remaining,
+                resetAt:userResult.resetAt,
+                deniedBy:'user'
+            };
+        }
+        return this.wrappedLimiter.isAllowed(req,options);
+    }
+}
