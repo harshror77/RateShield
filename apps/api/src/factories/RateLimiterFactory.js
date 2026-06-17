d4ea94640b26b2feec5c1240e472e00aaddb370d
Harsh Vardhan <harshror2006@gmail.com>
2026-06-17 15:31:46 +0530

    feat(api): implement rate limiter factory to dynamically build decorator stacks

---
+++ apps/api/src/factories/RateLimiterFactory.js
@@ -0,0 +1,49 @@
+import { TokenBucketLimiter, SlidingWindowLimiter, FixedWindowLimiter } from '../algorithms/index.js';
+import { IpLimiterDecorator, UserLimiterDecorator, PlanLimiterDecorator } from '../decorators/index.js';
+import { ALGORITHMS, planLimits, PLANS } from '../config/plans.config.js'; 
+
+const IP_LIMITS = { capacity: 5, refillRate: 1 };
+const USER_LIMITS = { capacity: 3, refillRate: 1 };
+
+export class RateLimiterFactory {
+  constructor() {
+    this.stackCache = new Map();
+  }
+
+  #createAlgorithm(clientConfig) {
+    const planId = clientConfig.planId || PLANS.FREE;
+    const limits = planLimits[planId] || planLimits[PLANS.FREE];
+
+    const baselineWindow = { maxRequests: limits.maxRequests, windowMs: limits.windowMs };
+    const baselineBucket = { 
+        capacity: limits.maxRequests, 
+        refillRate: Math.ceil(limits.maxRequests / (limits.windowMs / 1000)) 
+    };
+
+    switch (clientConfig.algorithm) {
+      case ALGORITHMS.SLIDING_WINDOW:
+        return new SlidingWindowLimiter(baselineWindow);
+      case ALGORITHMS.FIXED_WINDOW:
+        return new FixedWindowLimiter(baselineWindow);
+      case ALGORITHMS.TOKEN_BUCKET:
+      default:
+        return new TokenBucketLimiter(baselineBucket);
+    }
+  }
+
+  #buildStack(clientConfig) {
+    const innermost = this.#createAlgorithm(clientConfig);
+    const withIpCheck = new IpLimiterDecorator(innermost, IP_LIMITS);
+    const withUserCheck = new UserLimiterDecorator(withIpCheck, USER_LIMITS);
+    return new PlanLimiterDecorator(withUserCheck);
+  }
+
+  createLimiter(clientConfig) {
+    const algorithmKey = clientConfig.algorithm || ALGORITHMS.TOKEN_BUCKET;
+    if (this.stackCache.has(algorithmKey)) return this.stackCache.get(algorithmKey);
+    
+    const stack = this.#buildStack(clientConfig);
+    this.stackCache.set(algorithmKey, stack);
+    return stack;
+  }
+}
