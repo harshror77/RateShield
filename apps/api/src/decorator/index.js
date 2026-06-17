273ca84b4f397cb87992606138b8b1b8e5cfada1
Harsh Vardhan <harshror2006@gmail.com>
2026-06-17 13:52:32 +0530

    chore: reomve old decorator folder after rename

--- apps/api/src/decorator/index.js
+++
@@ -1,5 +0,0 @@
-
-export { BaseLimiterDecorator } from './BaseLimiterDecorator.js';
-export { IpLimiterDecorator } from './IpLimiterDecorator.js';
-export { UserLimiterDecorator } from './UserLimiterDecorator.js';
-export { PlanLimiterDecorator } from './PlanLimiterDecorator.js';
